2886942293a0ec5091c24dad84299b55c338fd23
icondenimStore <tn696199@gmail.com>
2026-09-21 19:25:59 +0700

    feat: update pipeline

--- rag/pipeline.py
+++ rag/pipeline.py
@@ -21,7 +21,7 @@ from langchain_core.embeddings import Embeddings
 from langchain_core.documents import Document
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS  
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
